fa61b3353ecd8c590adcd84f5217a3bdb0a00ee1
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-12-03 19:22:38 +0400

    add uploads

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -5,8 +5,8 @@ const path = require('path');
 const router = express.Router();
 
 router.post('/upload', uploadImage);
-router.get('/images', getImages);
-router.get('/images/:id', getImageById);
+router.get('/uploads', getImages);
+router.get('/uploads/:id', getImageById);
 router.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 module.exports = router;
