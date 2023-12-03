f17732af0948751f0d26c74f09ef3131adba495a
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-12-03 19:14:00 +0400

    add image db

--- src/controllers/imageUploadController.js
+++ src/controllers/imageUploadController.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
-
+const Image = require('../models/imageModel')
 // const upload = multer({
 //     storage: multer.memoryStorage(),
 //     limits: {fileSize: 5 * 1024 * 1024}
@@ -24,8 +24,10 @@ const uploadImage = async (req, res) => {
         const fileName = `${Date.now()}-${Math.round(Math.random() * 1000000)}.jpg`;
         const uploadPath = path.join('uploads', fileName);
         ensureDirectoryExists('uploads');
-
+        
         await image.toFile(uploadPath);
+        const newImage = new Image({fileName})
+        await newImage.save();
         res.status(200).json({message: "Image uploaded successfully", fileName});
     } catch (error) {
         console.error(error);
@@ -33,6 +35,27 @@ const uploadImage = async (req, res) => {
     }
 };
 
+const getImages = async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.json(images);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getImageById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const image = await Image.findById(id); 
+    res.json(image);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
 module.exports = {
-    uploadImage
+    uploadImage,
+    getImages,
+    getImageById
 };
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { uploadImage } = require('../controllers/imageUploadController');
+const { uploadImage, getImages, getImageById } = require('../controllers/imageUploadController');
 const path = require('path');
 
 const router = express.Router();
 
 router.post('/upload', uploadImage);
+router.get('/images', getImages);
+router.get('/images/:id', getImageById);
 router.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 module.exports = router;
